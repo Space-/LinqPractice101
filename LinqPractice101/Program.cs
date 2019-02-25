07f808309db60f165637807047041f6267495214
Bruce <gkm85663@gmail.com>
2019-02-26 01:29:51 +0800

    Create project and finish Linq1() function

--- LinqPractice101/Program.cs
+++ LinqPractice101/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LinqPractice101
 {
@@ -6,7 +7,24 @@ namespace LinqPractice101
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Console.WriteLine("Hello World!");
+            Linq1();
+            Console.ReadKey();
+        }
+
+        public static void Linq1()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var lowNums =
+                from n in numbers
+                where n < 5
+                select n;
+
+            Console.WriteLine("Numbers < 5:");
+            foreach (var num in lowNums)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
