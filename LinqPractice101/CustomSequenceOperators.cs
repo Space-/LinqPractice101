7fe5f2fdc9c6e1ec5485d52db4e30bf7863d5618
Bruce <gkm85663@gmail.com>
2019-08-17 00:28:31 +0800

    let this method become more extendable

--- LinqPractice101/CustomSequenceOperators.cs
+++ LinqPractice101/CustomSequenceOperators.cs
@@ -5,9 +5,9 @@ namespace LinqPractice101
 {
     public static class CustomSequenceOperators
     {
-        public static IEnumerable<T> Combine<T>(this IEnumerable<int> first, IEnumerable<int> second, Func<int, int, T> func)
+        public static IEnumerable<T> Combine<T>(this IEnumerable<T> first, IEnumerable<T> second, Func<T, T, T> func)
         {
-            using (IEnumerator<int> e1 = first.GetEnumerator(), e2 = second.GetEnumerator())
+            using (IEnumerator<T> e1 = first.GetEnumerator(), e2 = second.GetEnumerator())
             {
                 while (e1.MoveNext() && e2.MoveNext())
                 {
